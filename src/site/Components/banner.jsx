e43fa53a2a6b1dcbd8b5670d27b26c0e90d11917
zanimr <93678442+zanimr@users.noreply.github.com>
2025-07-04 07:15:29 -0300

    Adicionei o qrcode

--- src/site/Components/banner.jsx
+++ src/site/Components/banner.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 
-function Banner(){
-    return <section id="banner">
-        <div className="container">
-            <div className="row">
+function Banner() {
+    return (
+        <section id="banner">
+            <div className="container">
+                <div className="row">
 
-                <div className="col-lg-6">
-                    <h1>Dama de Copas NH</h1>
-                    <h4>Em breve em Novo Horizonte seu app adulto de Sex Shop, Lingeries e acessórios para sua diversão.</h4>
-                </div>
+                    <div className="col-lg-6 text-center text-lg-start">
+                        <h1>Dama de Copas NH</h1>
+                        <h4>
+                            Chegou em Novo Horizonte seu app adulto de Sex Shop, 
+                            Lingeries e acessórios para sua diversão.<br></br>
+                            Baixe agora!
+                        </h4>
+                        <img 
+                            src="Images/QrCodeLogo.png" 
+                            alt="Imagem adicional" 
+                            style={{ width: '30%', marginTop: '1rem' }} 
+                            className="mx-auto d-block d-lg-inline"
+                        />
+                    </div>
 
-                <div className="col-lg-6">
-                    <img src="Images/screenshot-crm.png" alt="99 CRM" />                        
+                    <div className="col-lg-6">
+                        <img 
+                            src="Images/screenshot-crm.png" 
+                            alt="99 CRM" 
+                            className="img-fluid"
+                        />
+                    </div>
+
                 </div>
-       
             </div>
-        </div>
-    </section>;
-  }
+        </section>
+    );
+}
 
 export default Banner;
